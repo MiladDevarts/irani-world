53a5be67a222028b92a8e0f0b8c68a98a9ec8618
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-01 13:18:11 +0400

    feat: add icon files

---
+++ components/Icons/Outline/index.ts
@@ -0,0 +1,25 @@
+
+import Password from "./Password";
+import Email from "./Email";
+import Dots from "./Dots";
+import Search from "./Search";
+
+import Resturant from "./Resturant";
+import Liquor from "./Liquor";
+import Furniture from "./Furniture";
+import CoffeeShop from "./CoffeeShop";
+
+import Sort from "./Sort";
+import Currency from "./Currency";
+import Message from "./Message";
+import Location from "./Location";
+
+
+import Star from "./Star";
+import Gem from "./Gem";
+import Crown from "./Crown"
+import Music from "./Music";
+import Call from "./Call";
+
+
+export {Call,Location,Message, Dots, Email, Password, Search, Resturant, Liquor, Furniture, CoffeeShop, Sort, Currency, Star ,Gem,Music,Crown }

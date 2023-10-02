573897857a6ef6dea03fbfaa3cf58b574d100692
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-02 13:48:10 +0400

    feat: add mobile overlay menu layout context

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 
 import ConatctOptions from "@/components/Contact/ConatctOptions";
 import ContactForm from "@/components/Contact/ContactForm";
+import OverlayMenu from "@/components/OverlayMenu";
 
 const page = () => {
   return (
@@ -11,12 +12,14 @@ const page = () => {
     <Head>
       <title>Irani World | Contact</title>
     </Head>
+    <OverlayMenu />
+
       <main className="flex flex-col lg:flex-row container">
-        <div className="w-full flex gap-x-4 pt-20 lg:pt-10">
-          <div className="w-1/2 h-full">
+        <div className="w-full flex flex-col lg:flex-row gap-x-16 pt-32 lg:pt-10">
+          <div className="w-full lg:w-1/2 h-full">
             <ConatctOptions />
           </div>
-          <div className="w-1/2 h-full">
+          <div className="w-full lg:w-1/2 h-full">
             <ContactForm />
           </div>
         </div>

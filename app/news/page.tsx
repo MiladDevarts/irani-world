22b634ba042e7a14a970bafd6e6c349850189f5e
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-06 01:34:34 +0400

    fix: craete news/bio sidebar layout

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -3,15 +3,18 @@ import React from "react";
 const page = () => {
   return (
     <main className="flex container">
-      <div className="flex order-2 w-3/4 min-h-screen">
-        <p className="text-black max-w-4xl">
-      
+      <div className="w-3/4 pt-28">
+        <p className="text-black">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          magni dolorum exercitationem architecto fugiat corporis, debitis
+          harum, sed ipsam eum labore omnis nam excepturi iusto molestiae
+          incidunt autem id molestias.
         </p>
       </div>
-      <div className="flex order-1 fixed top-0 w-1/4 bg-red min-h-screen text-white">
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum labore illo quaerat nesciunt ut eos praesentium tempore? Ea id sed et maxime assumenda deserunt commodi nostrum repudiandae cumque, sunt officia.
-       Quod adipisci illo, quaerat, quos fugit dicta laudantium architecto voluptatum repellendus impedit beatae eveniet facilis alias laboriosam voluptates soluta! Ex molestiae itaque nesciunt error voluptas excepturi, enim quam eius doloribus!
-       Natus totam, eveniet minima nesciunt repudiandae excepturi vitae soluta sed! Culpa ea ex odio vel voluptates officiis molestiae distinctio optio consequuntur hic nesciunt, fugit id autem sit. Asperiores, libero nemo.
+      <div className="relative w-1/4 h-screen">
+        <div className="fixed w-full h-full">
+          <p className="text-white"></p>
+        </div>
       </div>
     </main>
   );

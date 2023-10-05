4b39a7994a35046c8ba744654b451b03a4c49036
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-05 23:39:49 +0400

    fix: re-size & optimize mobile menu

--- components/Layout/Navigation.tsx
+++ components/Layout/Navigation.tsx
@@ -1,52 +1,46 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import Link from 'next/link'
+import React, { useState } from "react";
+import Link from "next/link";
 
-import { usePathname } from 'next/navigation'
-import Logo from '../Logo'
-import HamburgerNav from '../Hamburger'
+import { usePathname } from "next/navigation";
+import Logo from "../Logo";
+import HamburgerNav from "../Hamburger";
 
 const Navigation = () => {
+  const [menuItems, setMenuItesm] = useState([
+    { value: "Home", slug: "/" },
+    { value: "About us", slug: "/about" },
+    { value: "News", slug: "/news" },
+    { value: "Bio", slug: "/bio" },
+    { value: "FAQ", slug: "/faq" },
+    { value: "Help", slug: "/help" },
+    { value: "Terms", slug: "/terms" },
+    { value: "Contact us", slug: "/contact" },
+  ]);
 
-    const [menuItems, setMenuItesm] = useState([
-        { value: 'Home', slug: '/' },
-        { value: 'About us', slug: '/about' },
-        { value: 'News', slug: '/news' },
-        { value: 'Bio', slug: '/bio' },
-        { value: 'FAQ', slug: '/faq' },
-        { value: 'Help', slug: '/help' },
-        { value: 'Terms', slug: '/terms' },
-        { value: 'Contact us', slug: '/contact' },
-    ])
+  const pathname = usePathname();
 
-    const pathname = usePathname()
+  return (
+    <nav className="z-[1013] bg-white shadow-deep absolute bottom-[90%] lg:bottom-0 w-full h-[10vh] border-t border-white-shade-4 soft-shadow">
+      <div className="container flex gap-x-6 lg:gap-x-0 lg:justify-between items-center h-full">
+        <ul className="navigation hidden h-full lg:flex items-center gap-x-10 text-white-shade-10 font-opensans font-medium">
+          {menuItems.map((link) => {
+            const isActive = pathname.startsWith(link.slug);
+            return (
+              <li className={isActive ? "active" : "normal"}>
+                <Link href={link.slug}>{link.value}</Link>
+              </li>
+            );
+          })}
+        </ul>
+        <HamburgerNav />
+        <Link href={"/"}>
+          <Logo classes="lg:w-auto" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-    return (
-        <nav className='z-[1013] bg-white shadow-deep absolute bottom-[90%] lg:bottom-0 w-full h-[10vh] border-t border-white-shade-4 soft-shadow'>
-            <div className='container flex justify-between items-center h-full'>
-                <ul className='navigation hidden h-full lg:flex items-center gap-x-10 text-white-shade-10 font-opensans font-medium'>
-                    {
-                        menuItems.map((link) => {
-                            const isActive = pathname.startsWith(link.slug)
-                            return (
-                                <li className={isActive ? 'active' : 'normal'}>
-                                    <Link href={link.slug}>
-                                        {link.value}
-                                    </Link>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
-                <Link href={"/"}>
-                    <Logo />
-                </Link>
-                <HamburgerNav />
-            </div>
-        </nav>
-    )
-
-}
-
-export default Navigation
+export default Navigation;

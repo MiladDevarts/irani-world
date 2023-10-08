60ea76796a8e24a821768a36309e25071711144c
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-09 02:40:34 +0400

    fix: reize search on blog

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -19,22 +19,23 @@ const page = () => {
       <main className="container flex flex-col lg:flex-row">
         <div className="w-full lg:w-5/6 pt-16 lg:mt-0 mt-16">
           <div className="w-full flex flex-col gap-y-4 lg:flex-row justify-between items-center mb-10 ">
-            <div className="flex items-center gap-x-3 px-3 w-full lg:w-1/2 h-24 rounded-full border border-white-shade-4">
+            <div className="flex items-center gap-x-3 px-3 py-3 w-full lg:w-1/2 h-22 rounded-full border border-white-shade-4">
               <svg
-                width="83"
-                height="74"
-                viewBox="0 0 83 74"
+                width="58"
+                height="58"
+                viewBox="0 0 58 58"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <circle cx="41.5" cy="37.5" r="41.5" fill="#FAFAFA" />
+                <circle cx="29" cy="29" r="29" fill="#FAFAFA" />
                 <path
                   fill-rule="evenodd"
                   clip-rule="evenodd"
-                  d="M40.7835 24.05C33.8569 24.05 28.2418 29.5345 28.2418 36.3C28.2418 43.0655 33.8569 48.55 40.7835 48.55C47.71 48.55 53.3251 43.0655 53.3251 36.3C53.3251 29.5345 47.71 24.05 40.7835 24.05ZM26.0918 36.3C26.0918 28.3747 32.6695 21.95 40.7835 21.95C48.8974 21.95 55.4751 28.3747 55.4751 36.3C55.4751 39.8847 54.1294 43.1624 51.9045 45.6775L56.5936 50.2575C57.0134 50.6676 57.0134 51.3324 56.5936 51.7425C56.1738 52.1525 55.4931 52.1525 55.0733 51.7425L50.3842 47.1624C47.8093 49.3356 44.4535 50.65 40.7835 50.65C32.6695 50.65 26.0918 44.2253 26.0918 36.3Z"
+                  d="M28.5 19.75C23.6675 19.75 19.75 23.6675 19.75 28.5C19.75 33.3325 23.6675 37.25 28.5 37.25C33.3325 37.25 37.25 33.3325 37.25 28.5C37.25 23.6675 33.3325 19.75 28.5 19.75ZM18.25 28.5C18.25 22.8391 22.8391 18.25 28.5 18.25C34.1609 18.25 38.75 22.8391 38.75 28.5C38.75 31.0605 37.8111 33.4017 36.2589 35.1982L39.5303 38.4697C39.8232 38.7626 39.8232 39.2374 39.5303 39.5303C39.2374 39.8232 38.7626 39.8232 38.4697 39.5303L35.1982 36.2589C33.4017 37.8111 31.0605 38.75 28.5 38.75C22.8391 38.75 18.25 34.1609 18.25 28.5Z"
                   fill="black"
                 />
               </svg>
+
               <input
                 type="text"
                 placeholder="Search here..."

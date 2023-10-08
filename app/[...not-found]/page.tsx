d0ac1d88e1153e6cafddbcd3c0f3dfc9d78d30e4
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-08 21:18:33 +0400

    fix: edit 404 layout and image url

--- app/[...not-found]/page.tsx
+++ app/[...not-found]/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from "next/link";
+
 import OverlayMenu from "@/components/Overlay";
 
 export default function NotFoundPage() {
   return (
     <>
       <OverlayMenu />
-      <section className="w-full container flex flex-col gap-y-14 lg:flex-row-reverse lg:items-center lg:justify-between lg:mt-24">
+      <section className="w-full container flex flex-col sm:gap-x-10 gap-y-14 lg:flex-row-reverse lg:items-center lg:justify-between lg:mt-52">
         <div className="lg:mt-0 mt-40">
           <span className="text-black font-opensans px-6 py-3 border-[1px] border-white-shade-4 rounded-full font-semibold">
             Page not found
@@ -25,7 +26,7 @@ export default function NotFoundPage() {
             Back to homepage
           </Link>
         </div>
-        <img className="w-full lg:w-1/2" src="/Images/404/404.webp" alt="" />
+        <img className="w-full lg:w-1/2" src="/images/404/404.webp" alt="" />
       </section>
     </>
   );

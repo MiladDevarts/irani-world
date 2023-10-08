ddcd05ef997fdc57a6cb7c8f4e589c5d7d05f9fe
Milad Mohamadi <MiladDevarts@gmail.com>
2023-10-09 02:15:56 +0400

    feat: add search/filter section

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -5,6 +5,7 @@ import { NewsCardFullWidth, NewsCars } from "@/components/Blog";
 
 import OverlayMenu from "@/components/Overlay";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Irani World | News",
@@ -16,8 +17,51 @@ const page = () => {
     <>
       <OverlayMenu />
       <main className="container flex flex-col lg:flex-row">
-        <div className="w-full lg:w-5/6 pt-16 mt-20">
-          <div className="w-full flex flex-col lg:flex-row gap-y-6 gap-x-6 mb-10"></div>
+        <div className="w-full lg:w-5/6 pt-16 lg:mt-0 mt-16">
+          <div className="w-full flex flex-col gap-y-4 lg:flex-row justify-between items-center mb-10 ">
+            <div className="flex items-center gap-x-3 px-3 w-full lg:w-1/2 h-24 rounded-full border border-white-shade-4">
+              <svg
+                width="83"
+                height="74"
+                viewBox="0 0 83 74"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <circle cx="41.5" cy="37.5" r="41.5" fill="#FAFAFA" />
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M40.7835 24.05C33.8569 24.05 28.2418 29.5345 28.2418 36.3C28.2418 43.0655 33.8569 48.55 40.7835 48.55C47.71 48.55 53.3251 43.0655 53.3251 36.3C53.3251 29.5345 47.71 24.05 40.7835 24.05ZM26.0918 36.3C26.0918 28.3747 32.6695 21.95 40.7835 21.95C48.8974 21.95 55.4751 28.3747 55.4751 36.3C55.4751 39.8847 54.1294 43.1624 51.9045 45.6775L56.5936 50.2575C57.0134 50.6676 57.0134 51.3324 56.5936 51.7425C56.1738 52.1525 55.4931 52.1525 55.0733 51.7425L50.3842 47.1624C47.8093 49.3356 44.4535 50.65 40.7835 50.65C32.6695 50.65 26.0918 44.2253 26.0918 36.3Z"
+                  fill="black"
+                />
+              </svg>
+              <input
+                type="text"
+                placeholder="Search here..."
+                className="rounded-full text-lg text-black w-5/6 h-full focus:outline-none focus:border-none border-none outline-none"
+              />
+            </div>
+            <div className="flex gap-x-2 items-center">
+              <Link
+                className="flex justify-center items-center bg-blue text-white text-lg font-semibold font-outfit rounded-full border border-white-shade-4 px-10 h-16"
+                href={"/"}
+              >
+                Popular
+              </Link>
+              <Link
+                className="flex justify-center items-center text-white-shade-8 text-lg font-semibold font-outfit rounded-full border border-white-shade-4 px-10 h-16"
+                href={"/"}
+              >
+                Latest
+              </Link>
+              <Link
+                className="flex justify-center items-center text-white-shade-8 text-lg font-semibold font-outfit rounded-full border border-white-shade-4 px-10 h-16"
+                href={"/"}
+              >
+                Design
+              </Link>
+            </div>
+          </div>
           <div className="w-full flex flex-col lg:flex-row gap-y-6 gap-x-6 mb-10">
             <NewsCars
               readingTime={"  3 min read"}
@@ -119,7 +163,9 @@ const page = () => {
                       White Candle Black
                       <br />– White Deer Anons
                     </span>
-                    <span className="font-medium text-white-shade-8 text-lg">11 aug 2023</span>
+                    <span className="font-medium text-white-shade-8 text-lg">
+                      11 aug 2023
+                    </span>
                   </div>
                 </div>
               </div>

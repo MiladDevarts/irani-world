c0ffaa5fb725dc64eb722c7fb8e5527c407e002c
Milad Mohamadi <MiladDevarts@gmail.com>
2023-12-08 22:50:28 +0400

    fix: edit blog publish date

--- app/blog/wsa-riv-wps-partnership-innovative-ad-platform/page.tsx
+++ app/blog/wsa-riv-wps-partnership-innovative-ad-platform/page.tsx
@@ -64,7 +64,7 @@ const page = () => {
                     fill="#A6A6A6"
                   />
                 </svg>
-                September 4, 2020
+                October 18, 2020
               </div>
               <div className="flex items-center gap-x-3">
                 <img className="w-8" src="/images/bloggers/wsa.png" alt="" />

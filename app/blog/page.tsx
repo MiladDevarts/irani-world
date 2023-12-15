56dee15f6e014cff4428105cee1480228c218387
Milad Mohamadi <MiladDevarts@gmail.com>
2023-12-16 01:06:30 +0400

    fix: edit blog/search height on blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -19,7 +19,7 @@ const page = () => {
       <main className="container flex flex-col lg:flex-row">
         <div className="w-full lg:w-5/6 pt-16 lg:mt-0 mt-16">
           <div className="w-full flex flex-col gap-y-4 lg:flex-row justify-between items-center mb-10 ">
-            <div className="focus-within:border-black focus-within:border-2 transition-all flex items-center gap-x-3 px-3 py-3 w-full lg:w-1/2 h-22 rounded-full border-2 border-white-shade-4">
+            <div className="focus-within:border-black focus-within:border-2 transition-all flex items-center gap-x-3 px-3 py-3 w-full lg:w-[450px] h-16 rounded-full border-2 border-white-shade-4">
               <svg
                 width="58"
                 height="58"
@@ -44,7 +44,7 @@ const page = () => {
             </div>
             <div className="flex gap-x-2 items-center">
               <Link
-                className="gap-x-2 flex justify-center items-center bg-blue text-white text-lg font-semibold font-outfit rounded-full border border-white-shade-4 px-10 h-14"
+                className="gap-x-2 flex justify-center items-center bg-blue text-white text-lg font-semibold font-outfit rounded-full border border-white-shade-4 px-10 h-16"
                 href={"/"}
               >
                 <svg
@@ -62,7 +62,7 @@ const page = () => {
                 Popular
               </Link>
               <Link
-                className="hover:bg-black gap-x-2 hover:text-white transition-all duration-300 flex justify-center items-center text-white-shade-8 text-lg font-semibold font-outfit rounded-full border border-white-shade-4 px-10 h-14"
+                className="hover:bg-black gap-x-2 hover:text-white transition-all duration-300 flex justify-center items-center text-white-shade-8 text-lg font-semibold font-outfit rounded-full border border-white-shade-4 px-10 h-16"
                 href={"/"}
               >
                 <svg

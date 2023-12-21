844f872842e646629424e83cd9ce61e27125844e
Milad Mohamadi <MiladDevarts@gmail.com>
2023-12-21 14:50:53 +0400

    fix: edit 404/titles

--- app/[...not-found]/page.tsx
+++ app/[...not-found]/page.tsx
@@ -10,7 +10,7 @@ export default function NotFoundPage() {
       <section className="w-full container flex flex-col sm:gap-x-10 gap-y-14 lg:flex-row-reverse lg:items-center lg:justify-between lg:mt-52">
         <div className="lg:mt-0 mt-40">
           <span className="text-black font-opensans px-6 py-3 border-[1px] border-white-shade-4 rounded-full font-semibold">
-            Page not found
+            Page Not Found
           </span>
           <h1 className="text-4xl font-outfit mt-9 text-black font-semibold lg:text-6xl">
             Oh NO! Error 404
@@ -23,7 +23,7 @@ export default function NotFoundPage() {
             className="text-white bg-black py-4  px-14 font-opensans rounded-2xl"
             href={"/"}
           >
-            Back to homepage
+            Back To Home Page
           </Link>
         </div>
         <img className="w-full lg:w-1/2" src="/images/404/404.webp" alt="" />
